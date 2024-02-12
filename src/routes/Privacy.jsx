cb90ca95a36220adb04a3da71ec45d87dcf98f79
ValerieVichi <val.vichurina@gmail.com>
2024-02-12 15:31:29 -0700

    Add policy route
    	new file:   components/PrivacyPolicy.jsx
    	modified:   hooks/useVersion.jsx
    	new file:   images/android-chrome-512x512.png
    	new file:   images/apple-touch-icon.png
    	new file:   images/electricians/back-home-arrow.png
    	new file:   images/electricians/header-bckgr-min.jpg
    	new file:   images/electricians/policy.png
    	new file:   images/favicon.ico
    	new file:   images/plumbers/back-home-arrow.png
    	new file:   images/plumbers/policy.png
    	new file:   src/routes/Privacy.jsx
    	new file:   stylesheets/Privacy.scss

---
+++ src/routes/Privacy.jsx
@@ -0,0 +1,11 @@
+import Menu from "../../components/Menu";
+import PrivacyPolicy from "../../components/PrivacyPolicy";
+
+export default function Privacy() {
+  return (
+    <>
+      <Menu bottomLineColor="#D9D9D9" />
+      <PrivacyPolicy />
+    </>
+  );
+}
